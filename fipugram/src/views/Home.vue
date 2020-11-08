228811c5a788cf0fb1015e8379d6b05cc7a072ac
Marko Tomljanovic <markot@windowslive.com>
2020-11-08 23:26:34 +0100

    homework

--- fipugram/src/views/Home.vue
+++ fipugram/src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div class="jumbotron">
+  <h1 class="display-4">Welcome to .vue homework</h1>
+  <p class="lead">This is a simple homework witch also include bootstrap, a simple web page with small informations.</p>
+  <hr class="my-4">
+  <p>It is important to understand that if we want to progress we must be persistent. On the button below you can goole it</p>
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="https://www.google.com/search?sxsrf=ALeKk03xritKH8N-9CZ9cMRVeov45lmtPA%3A1604873740119&ei=DG6oX4b4BpLCUuOShKgL&q=progress+in+web+developing&oq=progress+in+web+developing&gs_lcp=CgZwc3ktYWIQAzIICCEQFhAdEB4yCAghEBYQHRAeMggIIRAWEB0QHjIICCEQFhAdEB4yCAghEBYQHRAeMgQIIRAKOgQIIxAnOgIIADoFCAAQsQM6CAgAELEDEIMBOggILhCxAxCDAToHCCMQ6gIQJzoJCCMQ6gIQJxATOgIILjoGCCMQJxATOgcIABCxAxBDOgoIABCxAxCDARBDOgQIABBDOgUILhCTAjoFCAAQywE6BggAEBYQHjoICAAQFhAKEB5Qj5UkWMbYJGCb2SRoA3ABeACAAZoBiAHeHJIBBDAuMjiYAQCgAQGqAQdnd3Mtd2l6sAEKwAEB&sclient=psy-ab&ved=0ahUKEwiGrsHJ_PPsAhUSoRQKHWMJAbUQ4dUDCA0&uact=5" role="button">Google it</a>
+  </p>
+</div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+
+
+
+
+<style>
+  @import './../assets/bootstrap.css';
+</style>
